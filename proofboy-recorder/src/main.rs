67c13c9534430b8519b2f5184e9961193715acc5
Willem Olding <willemolding@gmail.com>
2023-07-24 18:56:26 +1000

    working but a little janky

--- proofboy-recorder/src/main.rs
+++ proofboy-recorder/src/main.rs
@@ -1,13 +1,17 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use std::cell::RefCell;
+use std::rc::Rc;
+use bevy::{prelude::*, input::{keyboard::KeyboardInput, ButtonState}};
+use log;
 use rgy::{debug::NullDebugger, Config, Key as GBKey, Stream, System, VRAM_HEIGHT, VRAM_WIDTH};
 
 const SCALE: f32 = 2.0;
+const CYCLES_PER_FRAME: usize = 70224;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_gameboy)
-        .add_systems(Update, update)
+        .add_systems(Startup, (setup_screen, setup_gameboy))
+        .add_systems(Update, (update_gameboy, update_screen))
         .run();
 }
 
@@ -17,8 +21,104 @@ fn setup_gameboy(world: &mut World) {
     world.insert_non_send_resource(gb);
 }
 
-fn update(gb: NonSendMut<Gameboy>, keyboard: Res<Input<KeyCode>>, time: Res<Time>) {
-    
+fn setup_screen(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    // spawn all the pixels
+    for x in 0..VRAM_WIDTH {
+        for y in 0..VRAM_HEIGHT {
+            commands.spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::BLACK,
+                        custom_size: Some(Vec2::new(SCALE, SCALE)),
+                        ..default()
+                    },
+                    transform: Transform::from_translation(Vec3::new(
+                        SCALE * (x as f32),
+                        -SCALE * (y as f32),
+                        0.,
+                    )),
+                    ..default()
+                },
+                ScreenPosition { x, y },
+            ));
+        }
+    }
+}
+
+fn update_gameboy(
+    mut gb: NonSendMut<Gameboy>,
+    keys: Res<Input<KeyCode>>,
+) {
+    if keys.pressed(KeyCode::Up) {
+        gb.kbd.0.borrow_mut().up = true;
+    } else {
+        gb.kbd.0.borrow_mut().up = false;
+    }
+
+    if keys.pressed(KeyCode::Return) {
+        gb.kbd.0.borrow_mut().start = true;
+    } else {
+        gb.kbd.0.borrow_mut().start = false;
+    }
+
+    if keys.pressed(KeyCode::Down) {
+        gb.kbd.0.borrow_mut().down = true;
+    } else {
+        gb.kbd.0.borrow_mut().down = false;
+    }
+
+    if keys.pressed(KeyCode::Left) {
+        gb.kbd.0.borrow_mut().left = true;
+    } else {
+        gb.kbd.0.borrow_mut().left = false;
+    }
+
+    if keys.pressed(KeyCode::Right) {
+        gb.kbd.0.borrow_mut().right = true;
+    } else {
+        gb.kbd.0.borrow_mut().right = false;
+    }
+
+    if keys.pressed(KeyCode::Z) {
+        gb.kbd.0.borrow_mut().a = true;
+    } else {
+        gb.kbd.0.borrow_mut().a = false;
+    }
+
+    if keys.pressed(KeyCode::X) {
+        gb.kbd.0.borrow_mut().b = true;
+    } else {
+        gb.kbd.0.borrow_mut().b = false;
+    }
+
+    if keys.pressed(KeyCode::Space) {
+        gb.kbd.0.borrow_mut().select = true;
+    } else {
+        gb.kbd.0.borrow_mut().select = false;
+    }
+
+    for _ in 0..CYCLES_PER_FRAME {
+        gb.as_mut().sys.poll();
+    }
+}
+
+fn update_screen(gb: NonSend<Gameboy>, mut query: Query<(&ScreenPosition, &mut Sprite)>) {
+    for (screen_pos, mut sprite) in query.iter_mut() {
+        let p = gb.display.0.borrow()[screen_pos.x][screen_pos.y];
+        sprite.color = Color::rgb_u8(
+            (p & 0xffu32) as u8,
+            ((p >> 8) & 0xffu32) as u8,
+            ((p >> 16) & 0xffu32) as u8,
+        );
+    }
+}
+
+#[derive(Debug, Component)]
+struct ScreenPosition {
+    x: usize,
+    y: usize,
 }
 
 struct Gameboy {
@@ -34,7 +134,7 @@ impl Default for Gameboy {
         let cfg = Config::new();
         let hw = Hardware::new(display.clone(), kbd.clone());
 
-        let rom = b"xxx";//include_bytes!(env!("WABOY_ROM"));
+        let rom = include_bytes!("../../roms/pokemon-blue.gb");
 
         let sys = System::new(cfg, rom, hw, NullDebugger);
 
@@ -42,21 +142,13 @@ impl Default for Gameboy {
     }
 }
 
-impl Gameboy {
-    fn poll(&mut self) {
-        for _ in 0..3 {
-            self.sys.poll();
-        }
-    }
-}
+#[derive(Clone, Debug)]
+struct Display(Rc<RefCell<Vec<Vec<u32>>>>);
 
-#[derive(Debug, Clone)]
-struct Display(Vec<Vec<u32>>);
+#[derive(Clone, Debug)]
+struct Keyboard(Rc<RefCell<Inner>>);
 
-#[derive(Debug, Clone)]
-struct Keyboard(Inner);
-
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug)]
 struct Inner {
     up: bool,
     down: bool,
@@ -68,18 +160,18 @@ struct Inner {
     select: bool,
 }
 
-impl Display {
+impl Keyboard {
     fn new() -> Self {
-        Self(vec![
-            vec![0u32; VRAM_HEIGHT];
-            VRAM_WIDTH
-        ])
+        Self(Rc::new(RefCell::new(Inner::default())))
     }
 }
 
-impl Keyboard {
+impl Display {
     fn new() -> Self {
-        Self(Inner::default())
+        Self(Rc::new(RefCell::new(vec![
+            vec![0u32; VRAM_HEIGHT];
+            VRAM_WIDTH
+        ])))
     }
 }
 
@@ -99,37 +191,30 @@ impl rgy::Hardware for Hardware {
         let y = line;
 
         for (x, col) in buffer.iter().enumerate() {
-            self.display.0[x][y] = *col;
+            self.display.0.borrow_mut()[x][y] = *col;
         }
     }
 
     fn joypad_pressed(&mut self, key: GBKey) -> bool {
         match key {
-            GBKey::Up => self.kbd.0.up,
-            GBKey::Down => self.kbd.0.down,
-            GBKey::Left => self.kbd.0.left,
-            GBKey::Right => self.kbd.0.right,
-            GBKey::A => self.kbd.0.a,
-            GBKey::B => self.kbd.0.b,
-            GBKey::Start => self.kbd.0.start,
-            GBKey::Select => self.kbd.0.select,
+            GBKey::Up => self.kbd.0.borrow().up,
+            GBKey::Down => self.kbd.0.borrow().down,
+            GBKey::Left => self.kbd.0.borrow().left,
+            GBKey::Right => self.kbd.0.borrow().right,
+            GBKey::A => self.kbd.0.borrow().a,
+            GBKey::B => self.kbd.0.borrow().b,
+            GBKey::Start => self.kbd.0.borrow().start,
+            GBKey::Select => self.kbd.0.borrow().select,
         }
     }
 
     fn sound_play(&mut self, _stream: Box<dyn Stream>) {}
 
     fn clock(&mut self) -> u64 {
-        #[cfg(target_arch = "wasm32")]
-        {
-            Date::now() as u64 * 1000
-        }
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            let epoch = std::time::SystemTime::now()
-                .duration_since(std::time::UNIX_EPOCH)
-                .expect("Couldn't get epoch");
-            epoch.as_micros() as u64
-        }
+        let epoch = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .expect("Couldn't get epoch");
+        epoch.as_micros() as u64
     }
 
     fn send_byte(&mut self, _b: u8) {}
