b2a5c3e67a96c4cdbea72a3573de71db043ef331
Willem Olding <willemolding@gmail.com>
2023-08-11 17:18:34 +1000

    add removal of pendingMints once they are minted in subgraph

--- subgraph/generated/schema.ts
+++ subgraph/generated/schema.ts
@@ -12,9 +12,9 @@ import {
 } from "@graphprotocol/graph-ts";
 
 export class PendingMint extends Entity {
-  constructor(id: Bytes) {
+  constructor(id: string) {
     super();
-    this.set("id", Value.fromBytes(id));
+    this.set("id", Value.fromString(id));
   }
 
   save(): void {
@@ -22,36 +22,34 @@ export class PendingMint extends Entity {
     assert(id != null, "Cannot save PendingMint entity without an ID");
     if (id) {
       assert(
-        id.kind == ValueKind.BYTES,
-        `Entities of type PendingMint must have an ID of type Bytes but the id '${id.displayData()}' is of type ${id.displayKind()}`
+        id.kind == ValueKind.STRING,
+        `Entities of type PendingMint must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
       );
-      store.set("PendingMint", id.toBytes().toHexString(), this);
+      store.set("PendingMint", id.toString(), this);
     }
   }
 
-  static loadInBlock(id: Bytes): PendingMint | null {
+  static loadInBlock(id: string): PendingMint | null {
     return changetype<PendingMint | null>(
-      store.get_in_block("PendingMint", id.toHexString())
+      store.get_in_block("PendingMint", id)
     );
   }
 
-  static load(id: Bytes): PendingMint | null {
-    return changetype<PendingMint | null>(
-      store.get("PendingMint", id.toHexString())
-    );
+  static load(id: string): PendingMint | null {
+    return changetype<PendingMint | null>(store.get("PendingMint", id));
   }
 
-  get id(): Bytes {
+  get id(): string {
     let value = this.get("id");
     if (!value || value.kind == ValueKind.NULL) {
       throw new Error("Cannot return null for a required field.");
     } else {
-      return value.toBytes();
+      return value.toString();
     }
   }
 
-  set id(value: Bytes) {
-    this.set("id", Value.fromBytes(value));
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
   }
 
   get calldata(): Bytes {
--- subgraph/src/erc-1155-challengeable-mint.ts
+++ subgraph/src/erc-1155-challengeable-mint.ts
@@ -2,9 +2,10 @@ import {
   MintProposed,
 } from "../generated/ERC1155ChallengeableMint/ERC1155ChallengeableMint"
 import { PendingMint } from "../generated/schema"
+import { store } from '@graphprotocol/graph-ts'
 
 export function handleMintProposed(event: MintProposed): void {  
-  let entity = new PendingMint(event.transaction.hash)
+  let entity = new PendingMint(event.params.id.toHex())
   entity.calldata = event.transaction.input
   entity.txn_hash = event.transaction.hash
   entity.token_id = event.params.id
@@ -12,4 +13,8 @@ export function handleMintProposed(event: MintProposed): void {
   entity.timestamp = event.params.timestamp
 
   entity.save()
+}
+
+export function handleMinted(event: MintProposed): void {  
+  store.remove("PendingMint", event.params.id.toHex())
 }
